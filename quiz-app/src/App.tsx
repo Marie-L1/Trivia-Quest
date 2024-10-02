447dc07bb1691bcbd0fd7a5c67046f738432f71a
Maria Lukowich <mlukowich27@gmail.com>
2024-10-02 11:50:45 -0600

    styling the mobile homepage

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.scss'
+import HomePage from "./pages/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
   return (
     <>
-     
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
