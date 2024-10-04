86e3f5508111c903a0e24f7981a04809654e9e78
Maria Lukowich <mlukowich27@gmail.com>
2024-10-04 12:13:28 -0600

    starting the results page

--- quiz-app/src/pages/ResultsPage/ResultsPage.tsx
+++ quiz-app/src/pages/ResultsPage/ResultsPage.tsx
@@ -1,10 +1,23 @@
 import "./ResultPage.scss"
+import { useNavigate } from "react-router-dom"
 
 function ResultsPage() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  }
+
   return (
     <section className="results">
-        <div>
-
+        <div className="results__content">
+            <h1 className="results__score">{score}</h1>
+            
+            <h3 className="results__title">Results</h3>
+            <button
+            className="results__btn"
+            onClick={handleClick}
+            >Home</button>
         </div>
     </section>
   )
