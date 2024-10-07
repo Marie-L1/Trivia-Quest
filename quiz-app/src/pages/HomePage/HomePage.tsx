83fff7c6c2782de3d2df8a32fe022bcc98972f6a
Maria Lukowich <mlukowich27@gmail.com>
2024-10-07 13:10:51 -0600

    trying to fix the fonts

--- quiz-app/src/pages/HomePage/HomePage.tsx
+++ quiz-app/src/pages/HomePage/HomePage.tsx
@@ -17,7 +17,7 @@ function HomePage({onStart}: HomePageProps) {
     <section className="homepage">
         <div className="homepage__content">
             <h3 className="homepage__welcome">Welcome to</h3>
-            <h1 className="homepage__title">TRIVIAL QUEST</h1>
+            <h1 className="homepage__title">TRIVIA QUEST</h1>
             <img src={sword} className="homepage__img" alt="pixel sword"/>
             <button
             className="homepage__btn"
